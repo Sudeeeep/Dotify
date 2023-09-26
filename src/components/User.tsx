95ed69b173594dc748b5db54221c486b5caa6c0e
Sudeep <sudeepn15@gmail.com>
2023-09-26 18:57:36 +0530

    add Dotify logo

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { RiUser3Line } from "react-icons/ri";
 import { StateContext } from "../context/StateContext";
 import axios from "axios";
+import dotifyLogo from "../assets/images/spotify-2.svg";
 
 export const User = () => {
   const {
@@ -32,13 +33,22 @@ export const User = () => {
   }, []);
 
   return (
-    <div className="text-right pt-2 pr-4">
-      <button
-        className="bg-black p-3 border border-black rounded-full"
-        onClick={() => setMenuVisible(!menuVisible)}
-      >
-        <RiUser3Line />
-      </button>
+    <div className="pt-2 p-4">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <img className="w-10 cursor-pointer" src={dotifyLogo} alt="dotify" />
+          <h1 className="text-2xl cursor-pointer">Dotify</h1>
+        </div>
+        <div>
+          <button
+            className="bg-black p-3 border border-black rounded-full"
+            onClick={() => setMenuVisible(!menuVisible)}
+          >
+            <RiUser3Line />
+          </button>
+        </div>
+      </div>
+
       <div
         className={`bg-[#121212] flex flex-col absolute right-6  ${
           menuVisible ? "block" : "hidden"
