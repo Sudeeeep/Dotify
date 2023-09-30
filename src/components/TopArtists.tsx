8c86250b1c2b2c94b68440707e96a9dcaffdf83d
Sudeep <sudeepn15@gmail.com>
2023-09-30 14:00:18 +0530

    refactor TopArtists to make it resuable

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -1,8 +1,10 @@
 import { useContext, useEffect } from "react";
 import { StateContext } from "../context/StateContext";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { User } from "./User";
 
-export const TopArtists = () => {
+export const TopArtists = ({ home }: { home?: boolean }) => {
   const {
     state: { token, favouriteArtists },
     dispatch,
@@ -28,30 +30,45 @@ export const TopArtists = () => {
       });
   }, []);
 
-  return (
-    <div>
-      <div className="flex justify-between mt-12 mb-4">
-        <p className="text-xl">Your Top Artists</p>
-        <p className="cursor-pointer">Show more</p>
-      </div>
-      <div className="grid grid-cols-4 gap-6">
-        {favouriteArtists?.slice(0, 8).map((item, index) => (
-          <div key={index} className="">
-            <div className="flex flex-col gap-4 items-center">
-              <div className="relative">
-                <img
-                  src={item.artistImg}
-                  alt={item.artistName}
-                  className="w-44 h-44 rounded-full  cursor-pointer"
-                />
-              </div>
-              <p className="text-lg text-center cursor-pointer hover:underline">
-                {item.artistName}
-              </p>
-            </div>
+  if (favouriteArtists) {
+    return (
+      <div className={home ? "" : `col-span-3`}>
+        {!home && <User />}
+        <div
+          className={
+            home ? "" : `h-[75vh] max-h-full px-8 py-4 overflow-auto col-span-3`
+          }
+        >
+          <div className="flex justify-between mb-4">
+            <p className="text-xl">Your Top Artists</p>
+            {home && (
+              <Link to={"/top-artists"} className="cursor-pointer">
+                Show more
+              </Link>
+            )}
           </div>
-        ))}
+          <div className="grid grid-cols-4 gap-6">
+            {(home ? favouriteArtists.slice(0, 8) : favouriteArtists).map(
+              (item, index) => (
+                <div key={index} className="">
+                  <div className="flex flex-col gap-4 items-center">
+                    <div className="relative">
+                      <img
+                        src={item.artistImg}
+                        alt={item.artistName}
+                        className="w-44 h-44 rounded-full  cursor-pointer"
+                      />
+                    </div>
+                    <p className="text-lg text-center cursor-pointer hover:underline">
+                      {item.artistName}
+                    </p>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
