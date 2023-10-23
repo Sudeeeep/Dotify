5662609d493be8f76f273ebb42fe1ab463e6e5a9
Sudeep <sudeepn15@gmail.com>
2023-10-23 11:57:17 +0530

    add responsive AlbumDetails  page

--- src/components/AlbumDetails.tsx
+++ src/components/AlbumDetails.tsx
@@ -20,11 +20,11 @@ export const AlbumDetails = () => {
 
   if (loading) {
     return (
-      <div className="col-span-3 overflow-hidden">
+      <div className="col-start-1 row-start-1 col-span-4 lg:col-start-2 overflow-hidden">
         <User />
         <div className="h-[75vh] max-h-full overflow-auto">
           <div className="flex gap-4 items-end m-6">
-            <div className="w-56 h-56 bg-[#121212]"></div>
+            <div className="w-32 h-32 min-[450px]:w-36 min-[450px]:h-36 min-[540px]:w-44 min-[540px]:h-44 sm:w-56 sm:h-56 bg-[#121212]"></div>
             <div className="w-[75%] flex flex-col gap-4">
               <div className="p-10 bg-[#A7A7A7]"></div>
               <div className="w-1/2 p-2 bg-[#A7A7A7]"></div>
@@ -32,7 +32,7 @@ export const AlbumDetails = () => {
           </div>
 
           <div className="flex flex-col p-6 text-[#858383]">
-            <div className="grid grid-cols-[1rem_1fr_6rem] gap-4 px-4 border-b">
+            <div className="grid grid-cols-[1rem_1fr_4rem] gap-4 px-4 border-b">
               <div>#</div>
               <div>Title</div>
               <div>Duration</div>
@@ -40,7 +40,7 @@ export const AlbumDetails = () => {
             {new Array(3).fill("").map((_, index) => (
               <div
                 key={index}
-                className="grid grid-cols-[1rem_1fr_6rem] gap-4 p-4"
+                className="grid grid-cols-[1rem_1fr_4rem] gap-4 p-4"
               >
                 <div>{index + 1}</div>
                 <div className="flex gap-4">
@@ -62,7 +62,7 @@ export const AlbumDetails = () => {
 
   if (error) {
     return (
-      <div className="col-span-3 overflow-hidden">
+      <div className="col-start-1 row-start-1 col-span-4 lg:col-start-2 overflow-hidden">
         <User />
         <div className="px-8 py-4 col-span-3 mb-4">
           <div>
@@ -79,24 +79,24 @@ export const AlbumDetails = () => {
 
   if (selectedAlbumDetails) {
     return (
-      <div className="col-span-3 overflow-hidden">
+      <div className="col-start-1 row-start-1 col-span-4 lg:col-start-2 overflow-hidden">
         <User />
         <div ref={divRef} className="h-[75vh] max-h-full overflow-auto">
           <div className="flex gap-4 items-end m-6">
             <img
               src={selectedAlbumDetails.albumImg}
               alt="album thumbnail"
-              className="w-56 h-56"
+              className="w-32 min-[450px]:w-36 min-[540px]:w-44 sm:w-56"
             />
             <div className="flex flex-col gap-4">
-              <p className="text-sm mb-1 text-[#D3D4D6]">
+              <p className="text-xs min-[500px]:text-sm mb-1 text-[#D3D4D6]">
                 {selectedAlbumDetails.type}
               </p>
-              <h1 className="text-6xl font-bold">
+              <h1 className="text-xl min-[450px]:text-3xl min-[540px]:text-4xl min-[730px]:text-6xl sm:text-5xl font-bold">
                 {selectedAlbumDetails.albumName}
               </h1>
               <div>
-                <ul className="list-disc flex gap-6">
+                <ul className="list-disc flex gap-6 text-[0.5rem] min-[400px]:text-xs min-[500px]:text-sm sm:text-base">
                   <Link
                     to={`/artist/${selectedAlbumDetails.artists[0].id}`}
                     className="hover:underline"
@@ -123,7 +123,7 @@ export const AlbumDetails = () => {
           </div>
 
           <div className="flex flex-col p-6 text-[#858383]">
-            <div className="grid grid-cols-[1rem_1fr_6rem] gap-4 px-4 border-b">
+            <div className="grid grid-cols-[1rem_1fr_4rem] gap-4 px-4 border-b">
               <div>#</div>
               <div>Title</div>
               <div>Duration</div>
@@ -131,15 +131,15 @@ export const AlbumDetails = () => {
             {selectedAlbumDetails.tracks.map((track, index) => (
               <div
                 key={index}
-                className="grid grid-cols-[1rem_1fr_6rem] gap-4 p-4 hover:opacity-80 hover:bg-[#2d2d2d]"
+                className="grid grid-cols-[1rem_1fr_4rem] gap-4 p-4 hover:opacity-80 hover:bg-[#2d2d2d]"
               >
                 <div>{index + 1}</div>
                 <div className="flex gap-4">
                   <div className="flex flex-col">
-                    <p className="text-white hover:underline cursor-pointer">
+                    <p className="text-white hover:underline cursor-pointer line-clamp-2">
                       {track.trackName}
                     </p>
-                    <p className="text-sm">
+                    <p className="text-sm line-clamp-1">
                       {track.artists.length > 1
                         ? track.artists.map((artist, index) => {
                             return index === track.artists.length - 1 ? (
